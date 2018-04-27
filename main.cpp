41024197bf7cf45403de946877f731000d1dd481
Jago <jago.coric@hotmail.com>
2018-04-27 08:12:07 +0200

    Merge with master code with test branch code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include "NeuronLayer.h"
 
-float rng(flo min, flo max){
+flo rng(flo min, flo max){
     random_device rd; //Initializes random engine
     mt19937 gen(rd()); //Mersenne Twister 19937 generator, rng
     uniform_real_distribution<flo> dis(min, max); //uniform probability distribution
@@ -8,37 +8,33 @@ float rng(flo min, flo max){
 }
 int main() {
 
-    //performance test of a simulated training session
-    //random_device rd; //Initializes random engine
-    //mt19937 gen(rd()); //Mersenne Twister 19937 generator, rng
-    //uniform_real_distribution<float> dis(-1, 1); //uniform probability distribution
+int nneuron = 150;
+int ninputs = 728;
+layer tost(nneuron,ninputs); //constructor2 for initialization
+//performance test setup
 
-    
-    int nneuron = 150;
-    int ninputs = 728;
-    layer tost(nneuron,ninputs);
+//create vectors for input values
+vector<vector<fp> > ww; //weights
+ww.resize(nneuron);
+vector<fp> input; //simulated pixel data
+input.resize(nneuron);
+vector<fp> bb; //bias
+bb.resize(nneuron);
 
-    vector<vector<float*> > ww;
-    ww.resize(nneuron);
-    vector<float*> input;
-    input.resize(nneuron);
-    vector<float*> bb;
-    bb.resize(nneuron);
-
-    for(int k = 0; k < 10; ++k){
-        
-    vector<float> wo(ninputs);
-    vector<vector<float> > w(nneuron,wo);
-    vector<float> b(nneuron);
-    vector<float> in(ninputs);
-    vector<float*> tmp;
-    tmp.resize(wo.size());
-    vector<float*> tmp2;
+for(int k = 0; k < 10; ++k){
+//vectors of pointers 
+vector<flo> wo(ninputs);
+vector<vector<flo> > w(nneuron,wo);
+vector<flo> b(nneuron);
+vector<flo> in(ninputs);
+vector<fp> tmp;
+tmp.resize(wo.size());
+vector<fp> tmp2;
     tmp2.resize(in.size());
 
-    //convert the vectors of floats to vectors of pointers
-    for( int i = 0; i < w.size(); ++i){
-        for(int  j = 0; j < w[i].size(); ++j){
+    //convert the vectors of flos to vectors of pointers
+    for( int i = 0; i < (int) w.size(); ++i){
+        for(int  j = 0; j < (int) w[i].size(); ++j){
             w[i][j] = rng(-1,1);
             tmp[j] = &(w[i][j]);
         }
@@ -48,20 +44,18 @@ int main() {
     b[i] = rng(-1,1);
     bb[i] = &b[i];
     }
-    //Constructor2
 
     //update the parameters
-    tost.setWeights(ww);
-    tost.setBias(bb);
-    *tost.getWeights()[0][0];
-    //calculate output
-    *tost.resultFunc(input,false)[0];
+    tost.setWeights(ww); //update weights
+    tost.setBias(bb); //update bias
+    *tost.getWeights()[0][0]; //output weights
+    *tost.resultFunc(input,false)[0]; //neuron output
+    
+    //optional cout
     //cout << "nrUpdated Weight(0,0): " << *tost.getWeights()[0][0] << endl;
     //cout << "nrOutput(0,0): " << *tost.resultFunc(input,false)[0] << endl;
     //cout << "nrOutput(0,0): " << *tost(input,false)[0] << endl;
-
     //cout << "dsigmoid(0,0); " << tost.dsigmoid(input,false)[0] << endl;
-    //cout << k << endl;
     }
     return 0;
 }
